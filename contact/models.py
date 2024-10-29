5433df2aeda351651e94b2f224448707ceaef6b2
gugonunes <gugonunes@hotmail.com>
2024-10-28 22:38:59 -0300

    feat(contact/models): adicionando campo de imagem e configurando o MEDIA_URL e ROOT

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -9,6 +11,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateField(default=timezone.now())
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
